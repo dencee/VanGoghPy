23c3f5f8c4a8c5a984269307875ab2df1291b353
Daniel Commins <dencee@users.noreply.github.com>
2022-02-27 21:14:29 -0800

    working

---
+++ Brush.py
@@ -0,0 +1,87 @@
+if False:
+    from lib.Processing3 import *
+
+class Brush:
+    def __init__(self, painting):
+        self.painting = painting
+        self.x = 0
+        self.y = 0
+        self.bristles = list()
+
+        self.num_bristles = 200
+        self.brush_size = 20
+        self.max_brush_thickness = 20
+        self.brush_stagger = 6
+
+        self.initialize_bristles()
+
+    def initialize_bristles(self):
+        for i in range(self.num_bristles):
+            bristle_x = random(-self.brush_size, self.brush_size)
+            bristle_y = random(-self.brush_size, self.brush_size)
+            bristle_radius = random(self.max_brush_thickness)
+
+            self.bristles.append(Brush.Bristle(self, bristle_x, bristle_y, bristle_radius))
+
+    def update(self):
+        self.x += ((mouseX - self.x) / self.brush_stagger)
+        self.y += ((mouseY - self.y) / self.brush_stagger)
+
+        if self.x < 0:
+            self.x = 0
+        elif self.x > width:
+            self.x = width
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y > height:
+            self.y = height
+
+    def draw(self):
+        # Must run whether mouse is pressed or not to keep bristle
+        # x/y positions up to date
+        for bristle in self.bristles:
+            if mousePressed:
+                bristle.draw()
+
+            # Must be placed after draw
+            bristle.update()
+
+    class Bristle:
+        def __init__(self, brush, bx, by, br):
+            self.brush = brush
+            self.start_x = bx
+            self.end_x = bx
+            self.start_y = by
+            self.end_y = by
+            self.radius = br
+            self.start_radius = br
+
+        def update(self):
+            if self.end_x == (self.brush.x + self.start_x) and \
+               self.end_y == (self.brush.y + self.start_y):
+                self.radius += 1
+            else:
+                self.radius = self.start_radius
+
+            self.end_x = self.brush.x + self.start_x
+            self.end_y = self.brush.y + self.start_y
+
+        def draw(self):
+            pixel = self.brush.painting.get(int(self.end_x), int(self.end_y))
+            stroke(pixel)
+
+            if self.end_x == (self.brush.x + self.start_x) and \
+               self.end_y == (self.brush.y + self.start_y):
+                strokeWeight(self.radius / 50)
+                point(self.end_x, self.end_y)
+            else:
+                distance = dist(self.brush.x + self.start_x, self.brush.y + self.start_y,
+                                self.end_x, self.end_y)
+
+                # The bigger the distance, i.e. the faster the mouse is moved,
+                # the bigger and more coarse the line width
+                strokeWeight((self.radius * distance) / 50)
+                line(self.brush.x + self.start_x, self.brush.y + self.start_y,
+                     self.end_x, self.end_y)
+
--- VanGogh.py
+++ VanGogh.py
@@ -1,12 +1,49 @@
 if False:
     from lib.Processing3 import *
 
+from Brush import Brush
+
+global brush, canvas
+canvas = None
+brush = None
 
 def setup():
-    size(500, 500)
+    global brush, canvas
+    size(800, 500)
+
+    # starryNight.jpg painterOnRoad.jpg, wheatField.jpg, strawHatPortrait.jpg
+    canvas = loadImage("images/canvas.jpg")
+    painting = loadImage("images/starryNight.jpg")
+
+    canvas.resize(width, height)
+    painting.resize(width, height)
+
+    paintbrush_cursor = loadImage("images/paintbrushCur.png")
+    paintbrush_cursor.resize(22 * 2, 28 * 2)
+    cursor(paintbrush_cursor)
+    background(canvas)
+
+    brush = Brush(painting)
 
 def draw():
-    background(255)
-    fill(0)
-    ellipse(mouseX, mouseY, 50, 50)
+    brush.update()
+    brush.draw()
+    pass
+
+def keyPressed():
+    if key == 'r':
+        image(canvas, 0, 0)
+
+
+if __name__ == '__main__':
+    try:
+        __papplet__
+        #li = list(globals())
+        #for i in li:
+        #    print(i)
+        #print(PApplet.__dict__)
+        print('got')
+    except NameError:
+        import os
+        os.system('java -jar lib/processing-py.jar ' + __file__)
 
