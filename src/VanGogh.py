20578d97e40573ed20c5163b2cbc3706c1593a4f
Daniel Commins <dencee@users.noreply.github.com>
2022-02-27 22:22:46 -0800

    added instructions

--- src/VanGogh.py
+++ src/VanGogh.py
@@ -13,10 +13,7 @@ Brush.BRUSH_STAGGER = 6
 # Objective: Paint a Van Gogh painting
 #
 # 1. Create global variables for a paintbrush, canvas, and painting
-global brush, canvas, painting
-canvas = None
-brush = None
-painting = None
+
 
 def setup():
     size(800, 500)
@@ -24,39 +21,37 @@ def setup():
 
     # 2. Declare the global variables from step 1), example:
     # global brush, canvas, painting
-    global brush, canvas, painting
+
 
     # 3. Use the loadImage function to initialize the canvas AND
     # painting variables in the images folder, example:
     # canvas = loadImage("images/canvas.jpg")
-    canvas = loadImage("images/canvas.jpg")
-    painting = loadImage("images/starryNight.jpg")
+
 
     # 4. Call resize on the canvas and painting variables to resize
     # the width and height, example
     # canvas.resize(width, height)
-    canvas.resize(width, height)
-    painting.resize(width, height)
+
 
     # 5. Use the background function to set the canvas image as the background
     # Do you see the canvas image on the window?
-    background(canvas)
+
 
     # 6. Initialize the brush variable to a new Brush, example:
     # brush = Brush(painting)
-    # You won't see the paint brush until you complete the next step!
-    brush = Brush(painting)
+    # You won't see the paint until you complete the next step!
+
 
 def draw():
+    pass
     # 7. Call the brush's update and draw methods
     # Do you see the paint on the window when you hold down the mouse?
-    brush.update()
-    brush.draw()
+
 
 def keyPressed():
+    pass
     # 8. Reset the background canvas when the key variable is equal to 'r'
-    if key == 'r':
-        background(canvas)
+
 
 def initializeCursor():
     paintbrush_cursor = loadImage("images/paintbrushCur.png")
