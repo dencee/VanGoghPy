857550290a5ffcc226e041623feb445632afc648
Daniel Commins <dencee@users.noreply.github.com>
2022-03-04 15:16:17 -0800

    First draft

--- src/VanGogh.py
+++ src/VanGogh.py
@@ -13,50 +13,41 @@ Brush.BRUSH_STAGGER = 6
 
 # 1. Create global variables for a paintbrush, canvas, and painting
 # global paintbrush, canvas, painting
-global paintbrush, canvas, painting
 
 def setup():
     size(800, 500)
     initializeCursor()
 
-    # 2. Declare the global variables from step 1) in this function, example:
+    # 2. Re-declare the global variables from step 1) in this function, example:
     # global paintbrush, canvas, painting
-    global paintbrush, canvas, painting
 
     # 3. Use the loadImage function to initialize the canvas AND
     # painting variables in the images folder, example:
     # canvas = loadImage("../images/canvas.jpg")
-    canvas = loadImage("../images/canvas.jpg")
 
     # 4. Use the loadImage function to initialize the painting variable
-    painting = loadImage("../images/starryNight.jpg")
 
     # 5. Call resize on the canvas and painting variables to resize
     # the width and height
-    canvas.resize(width, height)
-    painting.resize(width, height)
 
     # 6. Use the background function to set the canvas image as the background
     # Do you see the canvas image on the window?
-    background(canvas)
 
     # 7. Initialize the brush variable to a new Brush, example:
     # paintbrush = Brush(painting)
     # You won't see the paint until you complete the next step!
-    paintbrush = Brush(painting)
+
 
 def draw():
     pass
     # 8. Call the brush's update and draw methods
     # Do you see the paint on the window when you hold down the mouse?
-    paintbrush.update()
-    paintbrush.draw()
+
 
 def keyPressed():
     pass
     # 9. Reset the background canvas when the key variable is equal to 'r'
-    if key == 'r':
-        background(canvas)
+
 
 # 10. Try painting the other paintings in the /images folder and changing the
 # paintbrush settings at the top of the file.
